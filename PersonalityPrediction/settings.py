d20e73ca1776e333c200a147b4af3f5d98f0d4ec
Anyone <anyone@mail.com>
2024-01-22 01:03:59 +0300

    The STATICFILES_DIRS setting should not contain the STATIC_ROOT setting

--- PersonalityPrediction/settings.py
+++ PersonalityPrediction/settings.py
@@ -150,9 +150,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+#
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

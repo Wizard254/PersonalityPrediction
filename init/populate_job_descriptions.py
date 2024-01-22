6e70def128f781e9aa010aa05a35d5119c2e5431
Anyone <anyone@mail.com>
2024-01-22 15:26:07 +0300

    load data more efficiently using django model bulk_create

--- init/populate_job_descriptions.py
+++ init/populate_job_descriptions.py
@@ -5,6 +5,25 @@ import pandas as pd
 def load(file, max_rows=-1):
     df = pd.read_csv(file)
     count = 0
+
+    items: list[JobDescription] = []
+
+    def buffer_insert(insert_all=False):
+        def helper(d_max=0):
+            if len(items) >= d_max:
+                JobDescription.objects.bulk_create(items)
+                items.clear()
+                pass
+            pass
+
+        if insert_all:
+            helper()
+            pass
+        else:
+            helper(100)
+            pass
+        pass
+
     for index, row in df.iterrows():
         job_title = row['job_title']
         category = row['category']
@@ -14,13 +33,18 @@ def load(file, max_rows=-1):
         jd.title = job_title
         jd.description = job_description
         jd.category = category
-        jd.save()
+
+        # jd.save()
+        buffer_insert()
+
         if count >= max_rows:
             break
             pass
         count += 1
         pass
 
+    # We may have some items in the list
+    buffer_insert(insert_all=True)
     pass
 
 

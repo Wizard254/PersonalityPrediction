3cecf7b3b12f37dd50b0f83bbf2145ba41469bb5
Anyone <anyone@mail.com>
2024-01-23 21:17:56 +0300

    add the 'jds' - Job descriptions search endpoint

--- resumeuploads/urls.py
+++ resumeuploads/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+from resumeuploads.views import JobDescriptionSearch
 
 urlpatterns = [
-
+    path('jds', JobDescriptionSearch.as_view())
 ]

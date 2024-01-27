4c21e2d357cf862ba0959cf70d7407c09553b234
Anyone <anyone@mail.com>
2024-01-27 13:54:48 +0300

    optimize imports

--- resumeuploads/models.py
+++ resumeuploads/models.py
@@ -1,12 +1,8 @@
 import dataclasses
 
-from django.db import models
-
 # Create your models here.
 from django.contrib.auth.models import User
-
-from django.conf import settings
-from pathlib import Path
+from django.db import models
 
 
 class Document(models.Model):

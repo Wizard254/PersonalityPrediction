ebc3ba0ca01f2215400e6541fa97fdb48680b289
Anyone <anyone@mail.com>
2024-01-28 20:51:55 +0300

    we don't need to run collect static everytime when debugging

--- PersonalityPrediction/settings.py
+++ PersonalityPrediction/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = Path(MEDIA_ROOT) / 'static/'
+STATIC_ROOT = BASE_DIR / "static" if LOCAL else Path(MEDIA_ROOT) / 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
